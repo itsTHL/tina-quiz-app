4f8166c0471a6ab22ca7689f509a4b5b1cd75011
Tina Huynh Le <huynhletina@gmail.com>
2023-12-18 17:25:40 +0100

    wrote new solution to create new question card with complete styling

--- js/form.js
+++ js/form.js
@@ -1,14 +1,13 @@
 const formMain = document.querySelector('[data-js="form-main"]');
 const form = document.querySelector('[data-js="form"]');
-// const formSubmitButton = document.querySelector(
-//   '[data-js="form-submit-button"]'
-// );
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
+
+  /*old solution before I realized innerHTML exists:
 
   const newQuestionCard = document.createElement("div");
   newQuestionCard.classList.add("question-card");
@@ -17,14 +16,43 @@ form.addEventListener("submit", (event) => {
   const newAnswer = document.createElement("h3");
   newQuestionCard.append(newAnswer);
   const newTag = document.createElement("button");
-  newTag.classList.add("btn-tag");
+  newTag.classList.add("btn-tags");
   newQuestionCard.append(newTag);
 
+  const newQuestion;
   newQuestion.textContent = "Your question: " + data["your-question"];
   newAnswer.textContent = "Your answer: " + data["your-answer"];
   newTag.textContent = data["your-tag"];
 
-  formMain.append(newQuestionCard);
+  */
+
+  const newQuestionSection = document.createElement("section");
+  newQuestionSection.classList.add("question-card");
+
+  const newQuestion = data["your-question"];
+  const newAnswer = data["your-answer"];
+  const newTag = data["your-tag"];
+
+  newQuestionSection.innerHTML = `
+<img
+          src="./resources/bookmark.png"
+          alt="icon of a bookmark"
+          class="bookmark"
+          data-js="bookmark-button"
+        />
+        <h2>${newQuestion}</h2>
+        <button class="show-answer" data-js="show-answer-button">
+          Show Answer
+        </button>
+        <h3 data-js="answer">${newAnswer}</h3>
+        <div class="tags">
+          <button class="btn-tags">${newTag}</button>
+          <button class="btn-tags">#tagtwo</button>
+          <button class="btn-tags">#tagthree</button>
+        </div>
+`;
+
+  formMain.append(newQuestionSection);
 
   // event.target.reset();
 });
