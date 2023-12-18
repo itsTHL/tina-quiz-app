f60d82f90cd9d9642715df54c9f658ad62b058fb
Tina Huynh Le <huynhletina@gmail.com>
2023-12-18 16:13:09 +0100

    create new question card from form data

--- js/form.js
+++ js/form.js
@@ -1,3 +1,4 @@
+const formMain = document.querySelector('[data-js="form-main"]');
 const form = document.querySelector('[data-js="form"]');
 // const formSubmitButton = document.querySelector(
 //   '[data-js="form-submit-button"]'
@@ -6,10 +7,24 @@ const form = document.querySelector('[data-js="form"]');
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const formElements = event.target.elements;
-
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
-  console.log(data);
+  const newQuestionCard = document.createElement("div");
+  newQuestionCard.classList.add("question-card");
+  const newQuestion = document.createElement("h2");
+  newQuestionCard.append(newQuestion);
+  const newAnswer = document.createElement("h3");
+  newQuestionCard.append(newAnswer);
+  const newTag = document.createElement("button");
+  newTag.classList.add("btn-tag");
+  newQuestionCard.append(newTag);
+
+  newQuestion.textContent = "Your question: " + data["your-question"];
+  newAnswer.textContent = "Your answer: " + data["your-answer"];
+  newTag.textContent = data["your-tag"];
+
+  formMain.append(newQuestionCard);
+
+  // event.target.reset();
 });
