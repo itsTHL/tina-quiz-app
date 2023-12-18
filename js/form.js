81b8c70dfd891f5163c7f50aad93aa7879a9629c
Tina Huynh Le <huynhletina@gmail.com>
2023-12-18 15:30:54 +0100

    read all data from form and convert to object

--- js/form.js
+++ js/form.js
@@ -5,4 +5,11 @@ const form = document.querySelector('[data-js="form"]');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  const formElements = event.target.elements;
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
 });
