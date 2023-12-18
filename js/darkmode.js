a9a62693239fb244fd058409e85b36bb3059f126
Tina Huynh Le <huynhletina@gmail.com>
2023-12-18 20:37:08 +0100

    create class for dark mode

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,6 +2,5 @@ const buttonToggle = document.querySelector('[data-js="dm-btn"');
 const bodyElement = document.querySelector('[data-js="body"');
 
 buttonToggle.addEventListener("click", () => {
-  console.log("working");
-  bodyElement.classList.toggle("dark");
+  document.body.classList.toggle("darkmode");
 });
