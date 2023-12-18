f2e9803f4fe2ce14fafb1981c782a5fdfd0ec9d4
Tina Huynh Le <huynhletina@gmail.com>
2023-12-18 19:24:34 +0100

    add event listeners to new created question card

--- js/form.js
+++ js/form.js
@@ -76,5 +76,32 @@ form.addEventListener("submit", (event) => {
 
   formMain.append(newQuestionSection);
 
-  // event.target.reset();
+  const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+
+  bookmarkButton.addEventListener("click", (event) => {
+    if (bookmarkButton.src.endsWith("bookmark.png")) {
+      bookmarkButton.src = "./resources/bookmark_filled.png";
+    } else {
+      bookmarkButton.src = "./resources/bookmark.png";
+    }
+  });
+
+  const showAnswerButton = document.querySelector(
+    '[data-js="show-answer-button"]'
+  );
+  const answer = document.querySelector('[data-js="answer"]');
+
+  answer.classList.add("hidden");
+
+  showAnswerButton.addEventListener("click", (event) => {
+    answer.classList.toggle("hidden");
+
+    if (showAnswerButton.innerText === "Show Answer") {
+      showAnswerButton.innerText = "Hide Answer";
+    } else {
+      showAnswerButton.innerText = "Show Answer";
+    }
+  });
+
+  event.target.reset();
 });
