b12b67ef207085650d7c6c6b0a87c3bada573414
Tina Huynh Le <huynhletina@gmail.com>
2023-12-18 11:04:06 +0100

    changed wrong bookmark icon on bookmark page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+
+bookmarkButton.addEventListener("click", (event) => {
+  bookmarkButton.src = "./resources/bookmark_filled.png";
+});
